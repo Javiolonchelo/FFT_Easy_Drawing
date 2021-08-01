cd97074d57f04123c7b508e1e42e10036a114d77
Javier Rodrigo López <javiolonchelo@gmail.com>
2021-08-01 19:12:14 +0200

    Imágenes PNG y JPG aceptadas

--- Dibujos_con_FFT.m
+++ Dibujos_con_FFT.m
@@ -1,4 +1,4 @@
-%% Preámbulo
+%% Preamble
 clear
 clc
 close all force
@@ -39,9 +39,16 @@ switch STATE
         INVERTED = false;
         TH = 127;
         STATE = 0;
-
-        File = ['Images/' uigetfile('Images/*.jpg')];
-        I = rgb2gray(imread(File));
+        
+        file = ['Images/' uigetfile('Images/*')];
+        
+        if strcmp(file(end - 2:end), 'png' )
+            I = imread(file);
+            I = rgb2gray(I);
+        elseif strcmp(file(end - 3:end), 'jpg' )
+            I = rgb2gray(imread(file));
+        end
+        
         LEFT = I;
         width = length(I(1, :));
         height = length(I(:, 1));
@@ -52,13 +59,13 @@ switch STATE
         aux2 = zeros(height, width);
         aux3 = zeros(height, width);
         aux4 = zeros(height, width);
-
+        
         fig_contour = figure('Position', ([200 200 1400 500]));
         showImages(fig_contour);
-
+        
         %% UI
         h = uifigure('Position', ([700 700 550 250]));
-
+        
         % SLIDERS
         % TH
         sld = uislider(h, 'Position', [15, 45, 200, 3], ...
@@ -74,22 +81,17 @@ switch STATE
             'ValueChangingFcn', @(sld, event) sld_3_irq(event, width, fig_contour));
         sld_4 = uislider(h, 'Position', [200, 110, 300, 3], ...
             'ValueChangingFcn', @(sld, event) sld_4_irq(event, width, fig_contour));
-
-        sld_1.Value = 0;
-        sld_2.Value = 0;
-        sld_3.Value = 0;
-        sld_4.Value = 0;
-
-        sld_1.Limits = [0 100];
-        sld_2.Limits = [0 100];
-        sld_3.Limits = [0 100];
-        sld_4.Limits = [0 100];
-
+        
+        sld_1.Value = 0; sld_1.Limits = [0 100];
+        sld_2.Value = 0; sld_2.Limits = [0 100];
+        sld_3.Value = 0; sld_3.Limits = [0 100];
+        sld_4.Value = 0; sld_4.Limits = [0 100];
+        
         % TEXT
         lbl = uilabel(h, 'Position', [245 25 180 30], ...
             'Text', 'La imagen rellena no debe tener huecos de color negro');
         lbl.WordWrap = 'on';
-
+        
         % BUTTONS
         btn_start = uibutton(h, 'Text', 'Empezar', ...
             'Position', [435, 30, 100, 22], ...
@@ -97,19 +99,19 @@ switch STATE
         btn_inverted = uibutton(h, 'Text', 'Invertir', ...
             'Position', [30, 150, 100, 22], ...
             'ButtonPushedFcn', @(btn, event) btn_inverted_irq(fig_contour));
-
+        
         % Waiting...
         waitfor(fig_contour)
         close(h)
-
+        
         % Second Switch Statement
         switch STATE
             case 'CONTOUR_START'
                 C = cell2mat(bwboundaries(RIGHT));
-
+                
                 eachCircle = C(:, 2);
                 y = C(:, 1);
-
+                
                 M = (width / 2 - eachCircle) + 1i * (height / 2 - y);
                 r = 1:length(abs(M));
                 figure
@@ -119,26 +121,26 @@ switch STATE
                 hold off
                 title('Parte real e imaginaria')
                 axis equal
-
+                
                 H = fftshift(fft(M));
                 L = length(H);
                 H = H / L;
-
+                
                 figure
                 plot(abs(H))
                 title('FFT shifted')
-
+                
                 if mod(L, 2) == 1
                     K = (1:L)' - L / 2 - 1/2;
                 else
                     K = (1:L)' - L / 2 - 1;
                 end
-
+                
                 sheet = [K, abs(H), angle(H)];
                 sheet = sortCoeff(sheet);
-                File = File(1:end - 4);
-                writematrix(sheet, strcat(File, '.xls'))
-
+                file = file(1:end - 4);
+                writematrix(sheet, strcat(file, '.xls'))
+                
                 %% Representación y generación del vídeo
                 p = figure;
                 set(gcf, 'Position', [0 0 1080 1920])
@@ -151,31 +153,31 @@ switch STATE
                 open(v);
                 t = 0;
                 aux = 0;
-
+                
                 % Allocating memory for saved data
-
+                
                 eje = 0:step:1;
                 L = length(0:step:2 * pi);
-
+                
                 eachCircle = 0:step:1;
-
+                
                 circle = zeros(L, L);
                 circleBox = zeros(length(eachCircle), L, coeffMaxAmount);
-
+                
                 centre = zeros(L, 1);
                 centreBox = zeros(L, coeffMaxAmount + 1); % Para incluir el origen
-
+                
                 contourLine = zeros(L, 1);
-
+                
                 for coeff = minCoeff:coeffMaxAmount % Una vez por coeficiente
                     t = t + 1;
-
+                    
                     for whole = 0:step:1 % Una vuelta completa
                         cla
                         whole_aux = round(whole / step + 1);
                         texto = 'Número de coeficientes: ' + string(t);
                         text(0, height / 2, texto);
-
+                        
                         % Centros (y radios)
                         if coeff ~= 1
                             centre(whole_aux) = sheet(coeff - 1, 2) * exp(1i * (sheet(coeff - 1, 1) * 2 * pi * whole + sheet(coeff - 1, 3)));
@@ -183,36 +185,36 @@ switch STATE
                         else
                             centre(whole_aux) = 0;
                         end
-
+                        
                         % Circunferencias
                         for eachCoeff = 1:coeff
-
+                            
                             if (eachCoeff == coeff) && (eachCoeff ~= 1)
                                 circle = centreBox(whole_aux, eachCoeff - 1) + sheet(eachCoeff - 1, 2) * exp(1i * 2 * pi * eachCircle);
                                 circleBox(:, whole_aux, eachCoeff) = circle;
                             end
-
+                            
                             plot(real(circleBox(round(eachCircle / step + 1), whole_aux, eachCoeff)), imag(circleBox(round(eachCircle / step + 1), whole_aux, eachCoeff)), 'Color', '#0c821a')
-
+                            
                         end
-
+                        
                         contourLine(whole_aux) = centreBox(whole_aux, coeff);
                         
                         plot(real(contourLine(1:whole_aux)), imag(contourLine(1:whole_aux)), '.', 'Color', 'red') % Centro de la última circunferencia
                         plot(real(centreBox(whole_aux, 1:coeff - 1)), imag(centreBox(whole_aux, 1:coeff - 1)), '.', 'Color', 'blue') % Resto de centros
                         plot(real(centreBox(whole_aux, 1:coeff)), imag(centreBox(whole_aux, 1:coeff)), 'Color', 'black') % Radios
-
+                        
                         % Capturar gráfico y escribir en archivo
                         frame = getframe(gcf);
                         writeVideo(v, frame);
-
+                        
                     end
-
+                    
                 end
-
+                
                 close(v);
         end
-
+        
 end
 
 %% IRQs
@@ -220,130 +222,130 @@ end
 % Sliders
 
 function sld_irq (sld, fig)
-    global TH LEFT aux0 INVERTED
-    TH = sld.Value;
+global TH LEFT aux0 INVERTED
+TH = sld.Value;
 
-    if INVERTED
-        aux0 = LEFT < TH;
-    else
-        aux0 = LEFT > TH;
-    end
+if INVERTED
+    aux0 = LEFT < TH;
+else
+    aux0 = LEFT > TH;
+end
 
-    aux0 = imfill(aux0, 'holes');
-    showImages(fig);
+aux0 = imfill(aux0, 'holes');
+showImages(fig);
 end
 
 function sld_1_irq(sld, height, fig)
-    global aux1
-    resetImages();
-    aux1(:) = 0;
+global aux1
+resetImages();
+aux1(:) = 0;
 
-    if (sld.Value ~= height)
-        aux1(round(height * (1 - sld.Value / 100)) + 1:height, :) = 1;
-    end
+if (sld.Value ~= height)
+    aux1(round(height * (1 - sld.Value / 100)) + 1:height, :) = 1;
+end
 
-    showImages(fig);
+showImages(fig);
 end
 
 function sld_2_irq(sld, height, fig)
-    global aux2
-    resetImages();
-    aux2(:) = 0;
+global aux2
+resetImages();
+aux2(:) = 0;
 
-    if (sld.Value ~= height)
-        aux2(1:round(height * sld.Value / 100), :) = 1;
-    end
+if (sld.Value ~= height)
+    aux2(1:round(height * sld.Value / 100), :) = 1;
+end
 
-    showImages(fig);
+showImages(fig);
 end
 
 function sld_3_irq(sld, width, fig)
-    global aux3
-    resetImages();
-    aux3(:) = 0;
+global aux3
+resetImages();
+aux3(:) = 0;
 
-    if (sld.Value ~= width)
-        aux3(:, 1:round(width * sld.Value / 100)) = 1;
-    end
+if (sld.Value ~= width)
+    aux3(:, 1:round(width * sld.Value / 100)) = 1;
+end
 
-    showImages(fig);
+showImages(fig);
 end
 
 function sld_4_irq(sld, width, fig)
-    global aux4
-    resetImages();
-    aux4(:) = 0;
+global aux4
+resetImages();
+aux4(:) = 0;
 
-    if (sld.Value ~= width)
-        aux4(:, round(width * (1 - sld.Value / 100)) + 1:width) = 1;
-    end
+if (sld.Value ~= width)
+    aux4(:, round(width * (1 - sld.Value / 100)) + 1:width) = 1;
+end
 
-    showImages(fig);
+showImages(fig);
 end
 
 % Buttons
 
 function btn_details_irq(fig)
-    global STATE
-    STATE = 'DETAILS';
-    close(fig);
+global STATE
+STATE = 'DETAILS';
+close(fig);
 end
 
 function btn_contour_irq(fig)
-    global STATE
-    STATE = 'CONTOUR';
-    close(fig);
+global STATE
+STATE = 'CONTOUR';
+close(fig);
 end
 
 function btn_exit_irq(fig)
-    close(fig);
+close(fig);
 end
 
 function btn_start_irq(fig_contour)
-    global STATE
-    STATE = 'CONTOUR_START';
-    close(fig_contour)
+global STATE
+STATE = 'CONTOUR_START';
+close(fig_contour)
 end
 
 function btn_inverted_irq(fig)
-    global INVERTED aux0
-    INVERTED = ~INVERTED;
-    aux0 = ~aux0;
-    showImages(fig);
+global INVERTED aux0
+INVERTED = ~INVERTED;
+aux0 = ~aux0;
+showImages(fig);
 end
 
 %% Other functions
 function showImages(fig)
-    global LEFT RIGHT aux0 aux1 aux2 aux3 aux4
-    RIGHT = aux0 | aux1 | aux2 | aux3 | aux4;
-    RIGHT = imfill(RIGHT, 'holes');
-    clf(fig)
-    subplot(121)
-    imshow(LEFT)
-    title('Imagen original')
-    subplot(122)
-    imshow(RIGHT)
-    title('Imagen rellena')
+global LEFT RIGHT aux0 aux1 aux2 aux3 aux4
+RIGHT = aux0 | aux1 | aux2 | aux3 | aux4;
+RIGHT = imfill(RIGHT, 'holes');
+clf(fig)
+subplot(121)
+imshow(LEFT)
+title('Imagen original')
+subplot(122)
+imshow(RIGHT)
+title('Imagen rellena')
 end
 
 function resetImages()
-    global I LEFT RIGHT
-    LEFT = I .* uint8((I | RIGHT));
+global I LEFT RIGHT
+LEFT = I .* uint8((I | RIGHT));
 end
 
 function LEFT = sortCoeff(sheet)
-    LEFT = sheet;
+LEFT = sheet;
 
-    for n = 1:length(sheet(:, 1))
-        index = sheet(n, 1);
-
-        if index > 0
-            index = 2 * index - 1;
-        elseif index < 0
-            index = 2 * abs(index);
-        end
-
-        LEFT(index + 1, :) = sheet(n, :);
+for n = 1:length(sheet(:, 1))
+    index = sheet(n, 1);
+    
+    if index > 0
+        index = 2 * index - 1;
+    elseif index < 0
+        index = 2 * abs(index);
     end
+    
+    LEFT(index + 1, :) = sheet(n, :);
+end
 
 end
